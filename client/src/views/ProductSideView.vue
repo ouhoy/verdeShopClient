38f2d2082ebfec94e51eaf13801fa083cb1ffd02
ouhoy <kimkinon@gmail.com>
2024-01-18 02:57:27 +0100

    Add danger dialog

--- client/src/views/ProductSideView.vue
+++ client/src/views/ProductSideView.vue
@@ -1,83 +1,32 @@
+<template>
+
+  <main class="mx-auto max-w-7xl mt-16">
+  <DangerDialog @takeAction="handleDelete" @toggle="updateDialogOpenState" :isOpen=dangerDialogOpen title="Delete Product" :item-id=1 action="Delete">Are you sure you want to delete the product? All of
+    your product data will be permanently removed. This action cannot be undone.
+  </DangerDialog>
+<MainButton @click="handleClick">Open</MainButton>
+  </main>
+
+
+</template>
+
 <script setup lang="ts">
+import DangerDialog from "@/components/dialogs/DangerDialog.vue";
+import {ref} from "vue";
+import MainButton from "@/components/buttons/MainButton.vue";
 
-import {onMounted, ref} from "vue";
-import type {Product} from "../../types";
-import {Menu, MenuButton, MenuItem, MenuItems} from "@headlessui/vue";
-import UserIcon from "@/assets/icons/UserIcon.vue";
-import { ChevronDownIcon, EllipsisVerticalIcon } from '@heroicons/vue/20/solid'
+const dangerDialogOpen =ref(false)
 
+function updateDialogOpenState(isOpen) {
+  dangerDialogOpen.value = isOpen
+}
 
+function handleDelete(id) {
+  console.log(id)
+}
 
-const products = ref<Product>()
-
-onMounted(async ()=>{
-  const data = await fetch("http://localhost:8080/v1/products/");
-  products.value = await data.json();
-})
+function handleClick() {
+  dangerDialogOpen.value = true;
+}
 
 </script>
-
-<template>
-  <main class="mt-20 mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 flex gap-20">
-
-    <div class="w-full rounded-lg border bg-card text-card-foreground shadow-sm" data-v0-t="card">
-      <div class="flex flex-col space-y-1.5 p-6"><h3
-          class="text-2xl font-semibold whitespace-nowrap leading-none tracking-tight">Products</h3></div>
-      <div class="p-6">
-        <div class="relative w-full overflow-auto">
-          <table class="w-full caption-bottom text-sm">
-            <thead class="[&amp;_tr]:border-b">
-            <tr class="border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted">
-              <th class="h-12 px-4 text-left align-middle font-medium text-muted-foreground [&amp;:has([role=checkbox])]:pr-0 w-[80px] hidden md:table-cell">
-                Image
-              </th>
-              <th class="h-12 px-4 text-left align-middle font-medium text-muted-foreground [&amp;:has([role=checkbox])]:pr-0 max-w-[150px]">
-                Name
-              </th>
-              <th class="h-12 px-4 text-left align-middle font-medium text-muted-foreground [&amp;:has([role=checkbox])]:pr-0 max-w-[150px]">
-                Price
-              </th>
-              <th class="h-12 px-4 text-left align-middle font-medium text-muted-foreground [&amp;:has([role=checkbox])]:pr-0">
-                Colors
-              </th>
-              <th class="h-12 px-4 text-left align-middle font-medium text-muted-foreground [&amp;:has([role=checkbox])]:pr-0">
-                Sizes
-              </th>
-
-              <th class="h-12 px-4 text-left align-middle font-medium text-muted-foreground [&amp;:has([role=checkbox])]:pr-0">
-               Options
-              </th>
-            </tr>
-            </thead>
-
-            <tbody class="[&amp;_tr:last-child]:border-0">
-            <tr v-for="product in products" class="border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted">
-
-              <td class="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0 hidden md:table-cell">
-                <img
-                  :src=product.thumbnail  alt="Product image"
-                  class="aspect-square rounded-md object-cover">
-              </td>
-
-              <td class="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0 font-medium">
-                {{product.name}}
-              </td>
-
-              <td class="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0">${{product.price}}</td>
-              <td class="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0">{{product.colors.toString().replaceAll(",", ", ")}}</td>
-              <td class="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0">{{product.sizes.toString().replaceAll(",", ", ")}}</td>
-              <td class="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0">
-
-
-              </td>
-
-            </tr>
-
-            </tbody>
-          </table>
-        </div>
-      </div>
-    </div>
-
-  </main>
-</template>
