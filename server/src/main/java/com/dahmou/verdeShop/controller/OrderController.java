fbd0d4641ace102dcc3b37da5cd410a9c97a42ec
ouhoy <kimkinon@gmail.com>
2024-01-20 04:57:06 +0100

    Implement order CRUD

--- server/src/main/java/com/dahmou/verdeShop/controller/OrderController.java
+++ server/src/main/java/com/dahmou/verdeShop/controller/OrderController.java
@@ -2,9 +2,11 @@ package com.dahmou.verdeShop.controller;
 
 
 import com.dahmou.verdeShop.model.Order;
+import com.dahmou.verdeShop.model.Product;
 import com.dahmou.verdeShop.repository.OrderCollectionRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -25,9 +27,31 @@ public class OrderController {
         return repository.getAllOrders();
     }
 
+
+    @GetMapping("/{id}")
+    public Order findById(@PathVariable int id){
+        Order order = repository.getOrder(id);
+        if (order == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return order;
+    }
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("")
     public void create(@RequestBody Order order) {
         repository.createOrder(order);
     }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @DeleteMapping("/{id}")
+    public void delete(@RequestBody @PathVariable int id) {
+        repository.deleteOrder(id);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PutMapping("/{id}")
+    public void update( @PathVariable int id, @RequestBody Order order) {
+        repository.updateOrder(id,order);
+    }
+
 }
--- server/src/main/java/com/dahmou/verdeShop/repository/OrderCollectionRepository.java
+++ server/src/main/java/com/dahmou/verdeShop/repository/OrderCollectionRepository.java
@@ -1,24 +1,16 @@
 package com.dahmou.verdeShop.repository;
 
-import com.dahmou.verdeShop.model.Address;
 import com.dahmou.verdeShop.model.Order;
-import com.dahmou.verdeShop.model.Product;
-import com.dahmou.verdeShop.util.ResultSetHelperEnum;
-import com.fasterxml.jackson.core.JsonEncoding;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
+import org.aspectj.weaver.ast.Or;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.postgresql.util.PGobject;
-
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Arrays;
 import java.util.List;
 
 @Repository
@@ -73,6 +65,35 @@ public class OrderCollectionRepository {
         } catch (Exception e) {
             // Handle the exception (log it or throw a specific exception)
             e.printStackTrace();
+        }
+    }
+
+    public void updateOrder(int id, Order order) {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            String sql = "UPDATE Orders SET  products=?, address=?, date=?, user_id=? WHERE id=?";
+            jdbcTemplate.update(sql, objectMapper.writeValueAsString(order.products()), objectMapper.writeValueAsString(order.address()), order.date(), order.userId(), id);
+
+        } catch (Exception e) {
+            // Handle the exception (log it or throw a specific exception)
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteOrder(int id) {
+        String sql = "DELETE FROM Orders WHERE id=?";
+        jdbcTemplate.update(sql, id);
+    }
+
+    public Order getOrder(int id) {
+        try {
+            String sql = "SELECT * FROM Orders WHERE id=?";
+            return jdbcTemplate.queryForObject(sql, new Object[]{id}, OrderCollectionRepository::mapRow);
+        }catch (EmptyResultDataAccessException e) {
+            // Handle empty result - throw a custom exception or return null
+            return null; // Or throw a custom exception
         }
     }
 
