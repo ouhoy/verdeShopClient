9d8da94755964243acf5f74cf992321fa39573a5
ouhoy <kimkinon@gmail.com>
2024-01-23 07:16:11 +0100

    Add production files

--- src/production.ts
+++ src/production.ts
@@ -1,3 +1,3 @@
-import * as process from "process";
+// import * as process from "process";
 
-export const SERVER_URL = process.env.VITE_PRODUCTION ? 'verdeshopserver-production-7ff5.up.railway.app' : `http://localhost:8080`;
+export const SERVER_URL = import.meta.env.VITE_PRODUCTION ? 'verdeshopserver-production-7ff5.up.railway.app' : `http://localhost:8080`;
