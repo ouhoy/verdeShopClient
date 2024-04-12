bbfc4ffa52a6e3b7da996c4418f5f5c30e9a2654
ouhoy <75227707+ouhoy@users.noreply.github.com>
2024-04-12 06:56:49 +0000

    change production uri

--- src/production.ts
+++ src/production.ts
@@ -1,3 +1,3 @@
 // import * as process from "process";
 
-export const SERVER_URL = import.meta.env.PROD ? 'https://verdeshopserver-production-7ff5.up.railway.app' : `http://localhost:8080`;
+export const SERVER_URL = import.meta.env.PROD ? 'https://verdeshopserver.onrender.com' : `http://localhost:8080`;
